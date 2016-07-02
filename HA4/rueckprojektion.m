e35c3756f92638369bdcd02f761abc495db74b92
Zhiwei Han <hanzw356255531@icloud.com>
2016-07-02 20:24:27 +0200

    3D-Konstruktion

--- HA4/rueckprojektion.m
+++ HA4/rueckprojektion.m
@@ -2,11 +2,14 @@ function repro_error = rueckprojektion(Korrespondenzen, P1, I2, T, R, K)
 % Diese Funktion berechnet die projizierten Punkte in Kamera 2 und den
 % mittleren Rueckprojektionsfehler
 P1Homo = [P1;ones(1, size(P1, 2))];
-stdProMatrix = [1, 0, 0, 0;0, 1, 0, 0;0, 0, 1, 0];
-xHomo = K * stdProMatrix * [R, T;zeros(1, size(R, 2)), 1] * P1Homo;
-P2 = zeros(2, size(xHomo, 2));
-for i = 1 : size(xHomo, 2)
-	P2(:, i) = int16(xHomo(1:2, i) / xHomo(3, i));
-end
+P2Homo = [R, T;zeros(1, size(R, 2)), 1] * P1Homo;
+
+newKorrespondenzen = [Korrespondenzen(3:4, :);Korrespondenzen(1:2, :)];
+E = achtpunktalgorithmus(newKorrespondenzen, K);
+[T1,R1,T2,R2] = TR_aus_E(E);
+[~, ~, lambda, ~] = rekonstruktion(T1,T2,R1,R2,newKorrespondenzen,K);
+P2App = P2Homo(1:3, :);
+P2Pro = reshape(P2App(:) ./ kron(lambda, [1,1,1]'), [3, size(P2App, 2)]);
+x2Pro = K * P2Pro;
 hold on;
 end
